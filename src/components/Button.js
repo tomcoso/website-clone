5abd444d1e6a4e168d45e61414694b3833f9cdd8
tomcoso <tomasdessy0@gmail.com>
2023-02-15 12:51:08 +0100

    introduce theme slice and setup initial community styling using theme and palette

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,20 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: var(--panel);
+  border: var(--action) solid 1px;
+  border-radius: 50px;
+  padding: 5px 10px;
+  font-size: 1rem;
+  color: var(--action);
+  height: min-content;
+`;
+
 const Button = ({ type = "button", action = undefined, children }) => {
   return (
-    <button type={type} onClick={action}>
+    <StyledButton type={type} onClick={action}>
       {children}
-    </button>
+    </StyledButton>
   );
 };
 
