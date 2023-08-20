3c44235e6c84b9e82620524ad7c1092b8c224c45
tomcoso <tomasdessy0@gmail.com>
2023-08-20 13:40:17 +1200

    change router to 6.4v

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reduxStore from "./redux/reduxStore";
 import { Provider } from "react-redux";
-import { Routes, Route } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import { Route, createRoutesFromElements, RouterProvider } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import CommunityMain from "./communities/CommunityMain";
@@ -14,32 +14,34 @@ import CommunityAdmin from "./communities/CommunityAdmin";
 import PostMain from "./posts/PostMain";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="submit" element={<SubmitPost />} />
+      <Route path="c">
+        <Route path=":community">
+          <Route index element={<CommunityMain />} />
+          <Route path="submit" element={<SubmitPost />} />
+          <Route path="admin" element={<CommunityAdmin />} />
+          <Route path="post">
+            <Route path=":postid" element={<PostMain />} />
+          </Route>
+        </Route>
+      </Route>
+      <Route path="u">
+        <Route path=":userid" />
+      </Route>
+    </Route>
+  )
+);
+
 root.render(
   <React.StrictMode>
     <Provider store={reduxStore}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="submit" element={<SubmitPost />} />
-            <Route path="c">
-              <Route path=":community">
-                <Route index element={<CommunityMain />} />
-                <Route path="submit" element={<SubmitPost />} />
-                <Route path="admin" element={<CommunityAdmin />} />
-                <Route path="post">
-                  <Route path=":postid" element={<PostMain />} />
-                </Route>
-              </Route>
-            </Route>
-            <Route path="u">
-              <Route path=":userid" />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
